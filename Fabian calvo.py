73e8e2c04111aa556683573bca2e748dfcca931f
AndresiOort <andresestebanortizjimenez@MacBook-Air-de-Andres-2.local>
2023-08-13 10:06:29 -0500

    Agregé el archivo

---
+++ Fabian calvo.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 25 19:19:32 2022
+
+@author: andresestebanortizjimenez
+"""
+
+lst = [{'a':0,'b':1,'c':2},
+       {'d':3,'c':4,'b':5},
+       {'a':5,'d':6,'e':1}]
+
+print(lst[1])
+print(lst[1]['c'])
